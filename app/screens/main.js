8e3539a8d8a998e353e5b77b3e2b583e22c67943
Herce, Alejandro(aherce) <aherce@paypal.com>
2018-06-02 14:12:25 -0500

    SocketIO test implementation

--- app/screens/main.js
+++ app/screens/main.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, StatusBar } from 'react-native';
-import { Container, Header, Content, List, ListItem, Text } from 'native-base';
+import { Container, Header, Content, List, ListItem, Text, Button } from 'native-base';
+
+// window.navigator.userAgent = "react-native";
+import io from 'socket.io-client';
 
 export class MainScreen extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+    // Creating the socket-client instance will automatically connect to the server.
+    // Query parameters can also be provided, either with the query option
+    // or directly in the url (example: http://localhost/users?token=abc)
+    this.socket = io('https://api.herce.co/live');
+
+    this.socket.on('connect', () => {
+      var room = '1234';
+      // Connected, let's sign-up for to receive messages for this room
+      this.socket.emit('room', {room: room}, function (answer) { console.log(answer); });
+    });
+  }
+
+  sendPress = () => {
+    this.socket.emit('chat', {room: '1234', sender: 'alex', message: 'Testing from Native'}, function (answer) { console.log(answer); });
+  }
+
   render() {
     return (
-        <Content>
-          <List>
-            <ListItem>
-              <Text>Simon Mignolet</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Nathaniel Clyne</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Dejan Lovren</Text>
-            </ListItem>
-          </List>
-        </Content>
+      <Content>
+        <List>
+          <ListItem>
+            <Text>Simon Mignolet</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Nathaniel Clyne</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Dejan Lovren</Text>
+          </ListItem>
+          <ListItem>
+            <Button onPress={this.sendPress}>
+              <Text>Click Me! </Text>
+            </Button>
+          </ListItem>
+        </List>
+      </Content>
     );
   }
 }
